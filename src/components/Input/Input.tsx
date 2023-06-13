5db0a071aa16233bdc5b5f93c54643cf95eaac1c
Ta Phong Kien <vietnam9com.com>
2023-06-13 14:37:39 +0700

    Validation form register dùng  react-hook-form với Yup

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,29 @@
-function Input() {
-    return <div>Input</div>;
+import type { RegisterOptions, UseFormRegister } from 'react-hook-form';
+
+interface Props {
+    className?: string;
+    type: React.HTMLInputTypeAttribute;
+    placeholder?: string;
+    autoComplete?: string;
+    register: UseFormRegister<any>;
+    name: string;
+    rules?: RegisterOptions;
+    errorMessage?: string;
+}
+
+function Input({ className, type, placeholder, autoComplete, register, name, rules, errorMessage }: Props) {
+    return (
+        <div className={className}>
+            <input
+                type={type}
+                className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+                placeholder={placeholder}
+                autoComplete={autoComplete}
+                {...register(name, rules)}
+            />
+            <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errorMessage}</div>
+        </div>
+    );
 }
 
 export default Input;
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,42 +1,63 @@
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { schema, Schema } from 'src/utils/schemas';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Input from 'src/components/Input';
+
+type FormData = Schema;
 
 function Register() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<FormData>({ resolver: yupResolver(schema) });
+
+    const handleSubmitForm = handleSubmit((data) => {
+        // console.log(data);
+    });
     return (
         <div className='bg-orange'>
-            <div className='mx-auto max-w-7xl px-4'>
+            <div className='container'>
                 <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
                     <div className='lg:col-span-2 lg:col-start-4'>
-                        <form className='rounded bg-white p-10 shadow-sm'>
+                        <form className='rounded bg-white p-10 shadow-sm' onSubmit={handleSubmitForm} noValidate>
                             <div className='text-2xl'>Đăng ký</div>
-                            <div className='mt-8'>
-                                <input
-                                    type='email'
-                                    name='email'
-                                    className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
-                                    placeholder='Email'
-                                />
-                                <div className='mt-1 min-h-[1rem] text-sm text-red-600'></div>
-                            </div>
-                            <div className='mt-3'>
-                                <input
-                                    type='password'
-                                    name='password'
-                                    className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
-                                    placeholder='Password'
-                                />
-                                <div className='mt-1 min-h-[1rem] text-sm text-red-600'></div>
-                            </div>
-                            <div className='mt-3'>
-                                <input
-                                    type='password'
-                                    name='confirm_password'
-                                    className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
-                                    placeholder='Confirm Password'
-                                />
-                                <div className='mt-1 min-h-[1rem] text-sm text-red-600'></div>
-                            </div>
-                            <div className='mt-3'>
-                                <button className='w-full bg-red-500 px-2 py-4 text-center text-sm uppercase text-white hover:bg-red-600'>
+
+                            <Input
+                                className='mt-8'
+                                type='email'
+                                placeholder='Email'
+                                register={register}
+                                name='email'
+                                errorMessage={errors.email?.message}
+                            />
+
+                            <Input
+                                className='mt-2'
+                                type='password'
+                                placeholder='Password'
+                                register={register}
+                                name='password'
+                                errorMessage={errors.password?.message}
+                                autoComplete='on'
+                            />
+
+                            <Input
+                                className='mt-2'
+                                type='password'
+                                placeholder='Confirm Password'
+                                register={register}
+                                name='confirm_password'
+                                errorMessage={errors.confirm_password?.message}
+                                autoComplete='on'
+                            />
+
+                            <div className='mt-2'>
+                                <button
+                                    type='submit'
+                                    className='w-full bg-red-500 px-2 py-4 text-center text-sm uppercase text-white hover:bg-red-600'
+                                >
                                     Đăng ký
                                 </button>
                             </div>
